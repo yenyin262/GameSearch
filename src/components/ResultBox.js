5f633d873486c7cc2c4bb7ca005f7fbf1caf73a2
Yen Yin Hing <yenyinf287@gmail.com>
2019-10-30 21:42:58 -0700

    implement styling and remove sidebar component

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -1,68 +1,49 @@
 import React from "react";
-import SideBar from "./SideBar";
 
 export default class ResultBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gameInfo: undefined
-    };
-  }
-
   render() {
     const { gamesResult } = this.props;
-    const { gameInfo } = this.state;
+
     return (
-      <React.Fragment>
-        <div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row"
+        }}
+      >
+        <div style={{ flex: "1 1 auto" }}>
           <div>
             {this.props.gamesResult[0] && (
               <p
                 style={{
                   fontSize: "16px",
                   fontWeight: "bold",
-                  marginTop: "10px",
-                  marginLeft: "29%"
+                  marginTop: "10px"
                 }}
               >
                 Games:
               </p>
             )}
-            {gamesResult.map(game => (
-              <li
-                onClick={() => this.setState({ gameInfo: game })}
-                key={game._id}
-                style={{
-                  border: "1px  solid black ",
-                  width: "25%",
-                  marginLeft: "29%",
-                  padding: "5px",
-                  marginTop: "15px",
-                  listStyle: "none"
-                }}
-              >
-                {game.name}
-              </li>
-            ))}
-          </div>
-          <div>
-            <div>
-              {gameInfo !== undefined ? (
-                <div
+            <ul style={{ padding: "0px" }}>
+              {gamesResult.map(game => (
+                <li
+                  onClick={() => this.props.onClick(game)}
+                  key={game._id}
                   style={{
-                    width: "100%",
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "flex-end"
+                    border: "1px  solid black ",
+                    padding: "5px",
+                    marginTop: "15px",
+                    listStyle: "none"
                   }}
                 >
-                  <SideBar game={gameInfo} />
-                </div>
-              ) : null}
-            </div>
+                  {game.name}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
